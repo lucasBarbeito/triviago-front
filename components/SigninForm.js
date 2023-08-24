974466636c16f515d2f3c5436d40deafc96023a7
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2023-08-24 13:35:11 -0300

    Sign in warning console

--- components/SigninForm.js
+++ components/SigninForm.js
@@ -6,20 +6,56 @@ import styles from '../styles/SigninForm.module.css';
 
 const SigninForm = () => {
 
-    const [password, setPassword] = useState(null);
-    const [confirmPassword, setConfirmPassword]  = useState(null);
+    const [name, setName]  = useState("");
+    const [surname, setSurname]  = useState("");
+    const [email, setEmail]  = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword]  = useState("");
 
+    function handleName(event){
+        setName(event.target.value)
+    }
+    function handleSurname(event){
+        setSurname(event.target.value)
+    }
     function handlePassword(event){
         setPassword(event.target.value)
     }
-
+    function handleEmail(event){
+        setEmail(event.target.value)
+    }
     function handleConfirmPassword(event){
         setConfirmPassword(event.target.value)
     }
 
-    function passwordState(event){
+    function signinState(event){
         event.preventDefault()
-        console.log(password == confirmPassword)
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+        if(name == ""){
+            console.log("El nombre no puede estar vacío.");
+            return
+        }
+
+        if(surname == ""){
+            console.log("El apellido no puede estar vacío.");
+            return
+        }
+
+        if (!email.match(emailPattern)) {
+            console.log("El formato del correo electrónico no es válido.");
+            return
+        }
+        if(password !== confirmPassword){
+            console.log("Las contraseñas no coinciden.");
+            return
+        }
+        if (password.length < 8) {
+            console.log("La contraseña debe tener al menos 8 caracteres.");
+            return
+        }
+        console.log("Creacion de cuenta exitoso");
+        return
     }
 
     const currentDate = new Date().toISOString().split('T')[0];
@@ -29,12 +65,12 @@ const SigninForm = () => {
 
             <div className={styles.labelContainer}>
                 <label className={styles.label}>Nombre</label>
-                <input className={styles.input} type="text" id="name" name="name" placeholder="Ingresa tu nombre" />
+                <input className={styles.input} type="text" id="name" name="name" placeholder="Ingresa tu nombre" onChange={handleName}/>
             </div>
 
             <div className={styles.labelContainer}>
                 <label className={styles.label}>Apellido</label>
-                <input className={styles.input} type="text" id="surname" name="surname" placeholder="Ingresa tu apellido" />
+                <input className={styles.input} type="text" id="surname" name="surname" placeholder="Ingresa tu apellido" onChange={handleSurname}/>
             </div>
 
             {/*CALENDARIO:*/}
@@ -48,7 +84,7 @@ const SigninForm = () => {
 
             <div className={styles.labelContainer}>
                 <label className={styles.label}>Email</label>
-                <input className={styles.input} type="email" id="email" name="email" placeholder="Ingresa tu email" />
+                <input className={styles.input} type="email" id="email" name="email" placeholder="Ingresa tu email" onChange={handleEmail}/>
             </div>
 
             <div className={styles.labelContainer}>
@@ -62,7 +98,7 @@ const SigninForm = () => {
             </div>
 
             <div className={styles.actionContainer}>
-                <button className={styles.button} onClick={passwordState}>Registrarte</button>
+                <button className={styles.button} onClick={signinState}>Registrarte</button>
                 <p className={styles.text}>¿Ya tienes una cuenta? <a href="/login" className={styles.link}>Inicia sesión</a></p>
             </div>
         </form>
