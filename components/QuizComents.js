e3dfa33d02a141badca81de53fa5bd155c57f2f5
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2023-08-29 12:28:11 -0300

    Cambio de pestaña

--- components/QuizComents.js
+++ components/QuizComents.js
@@ -5,7 +5,7 @@ import styles from '../styles/QuizComents.module.css';
 const QuizComents = () => {
 
     const [comment, setComment] = useState("");
-    const [openClasificación, setOpenClasificación] = useState(false);
+    const [openComment, setOpenComment] = useState(true);
 
     function handleComment(event){
         setComment(event.target.value)
@@ -23,34 +23,42 @@ const QuizComents = () => {
         }
     }
 
-    function handleClasificaciónOpen(){
-        setOpenClasificación(true)
+    function handleCommentBoxOpen(){
+        console.log(openComment)
+        setOpenComment(true)
     }
 
-    function handleClasificaciónClose(){
-        setOpenClasificación(false)
+    function handleCommentBoxClose(){
+        console.log(openComment)
+        setOpenComment(false)
     }
 
     return (
         <div className={styles.backgroundBox}>
             <div className={styles.componentBox}>
                 <div className={styles.dividerBox}>
-                    <button className={styles.titleComentsText} onClick={handleClasificaciónClose}>Comentarios</button>
-                    <button className={styles.titleRateText} onClick={handleClasificaciónOpen}>Clasificación</button>
+                    <button className={openComment ? styles.titleSelectedText : styles.titleNotSelected} onClick={handleCommentBoxOpen}>Comentarios</button>
+                    <button className={openComment ? styles.titleNotSelected : styles.titleSelectedText} onClick={handleCommentBoxClose}>Clasificación</button>
                 </div>
                 <div className={styles.lineBox}>
-                    <div className={styles.dividerComentLine}></div>
-                    <div className={styles.dividerClasificationLine}></div>
+                    <div className={openComment ? styles.dividerSelectedLine : styles.dividerNotSelectedLine}/>
+                    <div className={openComment ? styles.dividerNotSelectedLine : styles.dividerSelectedLine}/>
                 </div>
-                <div className={styles.comentBox}>
-                    <p className={styles.numberTextComents}>8 Comentarios</p>
-                    <input type="text" id="comment" name="comment" placeholder="Agrega un comentario..." className={styles.inputComment} onChange={handleComment}></input>
-                    <div className={styles.insertCommentLine}></div>
-                    <div className={styles.buttonsContainers}>
-                        <button className={styles.whiteButton} onClick={cancelComment}>Cancelar</button>
-                        <button className={styles.greenButton} onClick={logComment}>Comentar</button>
+                {openComment ?
+                    <div className={styles.comentBox} id="commentBox">
+                        <p className={styles.numberTextComents}>8 Comentarios</p>
+                        <input type="text" id="comment" name="comment" placeholder="Agrega un comentario..." className={styles.inputComment} onChange={handleComment}/>
+                        <div className={styles.insertCommentLine}/>
+                        <div className={styles.buttonsContainers}>
+                            <button className={styles.whiteButton} onClick={cancelComment}>Cancelar</button>
+                            <button className={styles.greenButton} onClick={logComment}>Comentar</button>
+                        </div>
                     </div>
-                </div>
+                    :
+                    <div className={styles.comentBox} id="classificationtBox">
+                        <p className={styles.numberTextComents}>Clasificación</p>
+                    </div>
+                }
             </div>
         </div>
     );
