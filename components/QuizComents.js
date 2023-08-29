7b7f6950a806c2dc09217549d89a8fb3bcdef2fc
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2023-08-29 11:39:38 -0300

    Comentar

--- components/QuizComents.js
+++ components/QuizComents.js
@@ -1,13 +1,42 @@
-import React from 'react';
+"use client"
+import {useState} from 'react';
 import styles from '../styles/QuizComents.module.css';
 
 const QuizComents = () => {
+
+    const [comment, setComment] = useState("");
+    const [openClasificación, setOpenClasificación] = useState(false);
+
+    function handleComment(event){
+        setComment(event.target.value)
+    }
+
+    function cancelComment(){
+        setComment("")
+        document.getElementById("comment").value = ""
+    }
+
+    function logComment(){
+        if(comment != "") {
+            cancelComment()
+            console.log(comment)
+        }
+    }
+
+    function handleClasificaciónOpen(){
+        setOpenClasificación(true)
+    }
+
+    function handleClasificaciónClose(){
+        setOpenClasificación(false)
+    }
+
     return (
         <div className={styles.backgroundBox}>
             <div className={styles.componentBox}>
                 <div className={styles.dividerBox}>
-                    <p className={styles.titleComentsText}>Comentarios</p>
-                    <p className={styles.titleRateText}>Clasificación</p>
+                    <button className={styles.titleComentsText} onClick={handleClasificaciónClose}>Comentarios</button>
+                    <button className={styles.titleRateText} onClick={handleClasificaciónOpen}>Clasificación</button>
                 </div>
                 <div className={styles.lineBox}>
                     <div className={styles.dividerComentLine}></div>
@@ -15,6 +44,12 @@ const QuizComents = () => {
                 </div>
                 <div className={styles.comentBox}>
                     <p className={styles.numberTextComents}>8 Comentarios</p>
+                    <input type="text" id="comment" name="comment" placeholder="Agrega un comentario..." className={styles.inputComment} onChange={handleComment}></input>
+                    <div className={styles.insertCommentLine}></div>
+                    <div className={styles.buttonsContainers}>
+                        <button className={styles.whiteButton} onClick={cancelComment}>Cancelar</button>
+                        <button className={styles.greenButton} onClick={logComment}>Comentar</button>
+                    </div>
                 </div>
             </div>
         </div>
