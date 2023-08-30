56bbe56a62752c00de75b844ffb912c3bf595853
Josue Riera <josue.riera@ing.austral.edu.ar>
2023-08-30 01:41:00 -0300

    import changes

--- profile/page.js
+++ profile/page.js
@@ -1,8 +1,8 @@
-import Typography from '@mui/material/Typography';
+  import Typography from '@mui/material/Typography';
 
-export default function Page() {
-    return ( <Typography textAlign="center">
-    Profile
-  </Typography>
-    )
-  }
+  export default function Page() {
+      return ( <Typography textAlign="center">
+      Profile
+    </Typography>
+      )
+    }
