285eb21d013b16500c241857037b35a0c955abed
Josue Riera <josue.riera@ing.austral.edu.ar>
2023-08-29 23:06:22 -0300

    x2

---
+++ quizNew/page.js
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography';
+
+export default function Page() {
+    return ( <Typography textAlign="center">
+    Quiz Result
+  </Typography>
+    )
+  }
---
+++ quizOnGoing/page.js
@@ -0,0 +1,8 @@
+import Typography from '@mui/material/Typography';
+
+export default function Page() {
+    return ( <Typography textAlign="center">
+    Quiz On Going
+  </Typography>
+    )
+  }
