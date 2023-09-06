55c44df77e33f67abacebdbb1b2681148d1ee1ec
lucaslovaglio <lucas.lovaglio@ing.austral.edu.ar>
2023-09-06 16:33:16 -0300

    feat QuizInfo - Integration with backend

--- app/quiz/[id]/result/page.js
+++ app/quiz/[id]/result/page.js
@@ -1,9 +1,38 @@
-import React from 'react';
+"use client";
+
+import React, {useEffect, useState} from 'react';
 import LoginPage from "../../../login/page";
 import QuizComents from "../../../../components/QuizComents";
 import ResponsiveAppBar from "@/components/ResponsiveAppBar";
+import { useRouter } from 'next/router';
+import axios from "axios";
+import API_URL from '@root/config';
+
 
 const ResultPage = () => {
+    const [quizData, setQuizData] = useState(null);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { id } = router.query; // Obtener el valor del parámetro id de la URL
+
+            try {
+                const response = await axios.get(API_URL + "/quiz/" + id);
+
+                if (response.status === 200) {
+                    setQuizData(response.data);
+                }
+            } catch (error) {
+                alert(error);
+            }
+        };
+
+        // Llamar a la función de carga de datos después de que la página se haya montado
+        fetchData();
+    }, [router.query]);
+
     return (
         <div>
             <ResponsiveAppBar />
