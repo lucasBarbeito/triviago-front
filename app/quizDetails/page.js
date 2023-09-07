bd02527bfbcb541a0ba7711f41edcddd65ab8795
Josue Riera <josue.riera@ing.austral.edu.ar>
2023-09-07 00:12:55 -0300

    error de superpocision y Radio simple choice soluc

--- app/quizDetails/page.js
+++ app/quizDetails/page.js
@@ -3,19 +3,31 @@ import ResponsiveAppBar from '@/components/ResponsiveAppBar';
 import QuizDetails from '@/components/QuizDetails';
 
 const MyQuizComponent = () => {
-    const question1 = "Pregunta 1";
-    const answers1 = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"];
-    const question2 = "Pregunta 2";
-    const answers2 = ["Lionel", "Andrés", "Messi", "Cuccittini"];
-    const multipleCorrectAnswers = true; // true = multiple, false = 1 sola respuesta correcta
+  const question1 = "Pregunta 1";
+  const answers1 = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod"
+  ];
 
-    return (
-        <div>
-            <ResponsiveAppBar/>
-            <QuizDetails question={question1} answers={answers1} multipleCorrectAnswers={false} />
-            <QuizDetails question={question2} answers={answers2} multipleCorrectAnswers={multipleCorrectAnswers} />
-        </div>
-    );
+  const question2 = "Pregunta 2";
+  const answers2 = [
+    "Lionel",
+    "Andrés",
+    "Messi",
+    "Cuccittini"
+  ];
+
+  const multipleCorrectAnswers = true; // true = multiple, false = 1 sola respuesta correcta
+
+  return (
+    <div>
+      <ResponsiveAppBar />
+      <QuizDetails question={question1} answers={answers1} multipleCorrectAnswers={false} />
+      <QuizDetails question={question2} answers={answers2} multipleCorrectAnswers={multipleCorrectAnswers} />
+    </div>
+  );
 };
 
 export default MyQuizComponent;
