1ca422d1a6f8cfff77d86d1ca77467c2731d2966
AAugurusa <95092029+AAugurusa@users.noreply.github.com>
2023-09-27 23:18:53 -0300

    Bug fixes

--- components/CommentComponent.js
+++ components/CommentComponent.js
@@ -1,177 +1,215 @@
 "use client";
-import React, { useEffect, useState } from 'react';
-import { Box, Card, CardContent, IconButton, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Box, Card, CardContent, IconButton, Slide, Snackbar, Typography} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
-import { useRequestService } from "@/service/request.service";
+import {useRequestService} from "@/service/request.service";
 import styles from '../styles/CommentComponent.module.css';
+import {Alert} from "@mui/lab";
 
 const currentDate = new Date();
-const formattedDateTime = currentDate.toLocaleString(); 
+const formattedDateTime = currentDate.toLocaleString();
 
-const CommentComponent = ({ id, content, authorEmail, likes, handleDeleteComment, replyToComment, quizId, replies }) => {
-  const service = useRequestService();
-  const [likeCount, setLikeCount] = useState(likes);
-  const [replyText, setReplyText] = useState('');
-  const [responses, setResponses] = useState(replies);
-  const [liked, setLiked] = useState(false);
-  const [disliked, setDisLiked] = useState(false);
+const CommentComponent = ({id, content, authorEmail, likes, handleDeleteComment, replyToComment, quizId, replies}) => {
+    const service = useRequestService();
+    const [likeCount, setLikeCount] = useState(likes);
+    const [replyText, setReplyText] = useState('');
+    const [responses, setResponses] = useState(replies);
+    const [liked, setLiked] = useState(false);
+    const [disliked, setDisLiked] = useState(false);
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState("ERROR");
 
-  const handleLikeClick = async () => {
-    if (!liked) {
-      try {
-        await service.likeComment(id);
-        setLikeCount(likeCount + 1);
-        setLiked(true);
-        if (disliked) {
-          setDisliked(false);
+    const handleLikeClick = async () => {
+        if (!liked) {
+            try {
+                await service.likeComment(id);
+                setLikeCount(likeCount + 1);
+                setLiked(true);
+                if (disliked) {
+                    setDisliked(false);
+                }
+            } catch (error) {
+                console.error('Hubo un error, por favor intenta más tarde', error);
+            }
+        } else {
+            try {
+                await service.removeLikeComment(id);
+                setLikeCount(likeCount - 1);
+                setLiked(false);
+            } catch (error) {
+                console.error('Hubo un error, por favor intenta más tarde', error);
+            }
         }
-      } catch (error) {
-        console.error('Hubo un error, por favor intenta más tarde', error);
-      }
-    } else {
-      try {
-        await service.removeLikeComment(id);
-        setLikeCount(likeCount - 1);
-        setLiked(false);
-      } catch (error) {
-        console.error('Hubo un error, por favor intenta más tarde', error);
-      }
-    }
-  };
+    };
 
-  const handleDislikeClick = async () => {
-    if (!disliked) {
-      try {
-        await service.dislikeComment(id);
-        setLikeCount(likeCount - 1);
-        setDisliked(true);
-        if (liked) {
-          setLiked(false);
+    const handleDislikeClick = async () => {
+        if (!disliked) {
+            try {
+                await service.dislikeComment(id);
+                setLikeCount(likeCount - 1);
+                setDisliked(true);
+                if (liked) {
+                    setLiked(false);
+                }
+            } catch (error) {
+                console.error('Hubo un error, por favor intenta más tarde', error);
+            }
+        } else {
+            try {
+                await service.removeDislikeComment(id);
+                setLikeCount(likeCount + 1);
+                setDisliked(false);
+            } catch (error) {
+                console.error('Hubo un error, por favor intenta más tarde', error);
+            }
         }
-      } catch (error) {
-        console.error('Hubo un error, por favor intenta más tarde', error);
-      }
-    } else {
-      try {
-        await service.removeDislikeComment(id);
-        setLikeCount(likeCount + 1);
-        setDisliked(false);
-      } catch (error) {
-        console.error('Hubo un error, por favor intenta más tarde', error);
-      }
+    };
+
+    const handleCancel = () => {
+        setReplyText('');
+    };
+
+
+    const handleReply = async () => {
+        const trimmedReplyText = replyText.trim();
+        if (trimmedReplyText !== '') {
+            if (trimmedReplyText.length <= 255) {
+                try {
+                    const aux = await service.logComment({parentCommentId: id, content: replyText, quizId: quizId})
+                    setResponses([...responses, aux]);
+                    setReplyText('');
+                } catch (e) {
+                    console.error(e)
+                }
+            } else {
+                setMessage("La respuesta no puede tener mas de 255 caracteres");
+                setOpen(true);
+            }
+        }
+    };
+
+    useEffect(() => {
+        console.log(likeCount)
+    }, [])
+
+    function handleAnswer(event) {
+        setReplyText(event.target.value)
+        event.target.style.height = '22px'
+        event.target.style.height = (event.target.scrollHeight + 1) + 'px'
     }
-  };
 
-  const handleCancel = () => {
-    setReplyText('');
-  };
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
 
-  const handleReply = async () => {
-    if (replyText !== '') {
-      const aux = await service.logComment({parentCommentId:id, content:replyText, quizId: quizId})
-      setResponses([...responses, aux]);
-      setReplyText('');
-    }
-  };
-
-  useEffect(()=> {
-    console.log(likeCount)
-  },[])
-
-  return (
-    <Card variant="outlined" className={styles.componentBox}>
-      <CardContent>
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Box>
-            <Typography variant="username" className={styles.userNameText}>
-              {authorEmail}
-            </Typography>
-            <Typography variant="date" className={styles.dateText}>
-              {formattedDateTime}
-            </Typography>
-          </Box>
-          <Box>
-            <IconButton aria-label="Eliminar comentario" color="error" sx={{ position: 'relative', zIndex: 0 }} onClick={handleDeleteComment}>
-              <DeleteIcon />
-            </IconButton>
-          </Box>
-        </Box>
-        <textarea readOnly={true} className={styles.text}>
+    return (
+        <>
+            <Card variant="outlined" className={styles.componentBox}>
+                <CardContent>
+                    <Box display="flex" justifyContent="space-between" alignItems="center">
+                        <Box>
+                            <Typography variant="username" className={styles.userNameText}>
+                                {authorEmail}
+                            </Typography>
+                            <Typography variant="date" className={styles.dateText}>
+                                {formattedDateTime}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <IconButton aria-label="Eliminar comentario" color="error"
+                                        sx={{position: 'relative', zIndex: 0}} onClick={handleDeleteComment}>
+                                <DeleteIcon/>
+                            </IconButton>
+                        </Box>
+                    </Box>
+                    <textarea readOnly={true} className={styles.text}>
           {content}
         </textarea>
-        <div className={styles.likesContainer}>
-          <IconButton aria-label="Me gusta" onClick={handleLikeClick} color={liked ? 'primary' : 'default'} disabled={liked} >
-            <ThumbUpIcon />
-          </IconButton>
-          <Typography variant="body2" className={styles.likeCount}>
-            {likeCount}
-          </Typography>
-          <IconButton aria-label="No me gusta" onClick={handleDislikeClick} color={disliked ? 'primary' : 'default'} disabled={disliked} >
-          <ThumbDownIcon color={disliked ? 'primary' : 'default'} />
-          </IconButton>
-        </div>
-        <div className={styles.commentBox}>
+                    <div className={styles.likesContainer}>
+                        <IconButton aria-label="Me gusta" onClick={handleLikeClick}
+                                    color={liked ? 'primary' : 'default'} disabled={liked}>
+                            <ThumbUpIcon/>
+                        </IconButton>
+                        <Typography variant="body2" className={styles.likeCount}>
+                            {likeCount}
+                        </Typography>
+                        <IconButton aria-label="No me gusta" onClick={handleDislikeClick}
+                                    color={disliked ? 'primary' : 'default'} disabled={disliked}>
+                            <ThumbDownIcon color={disliked ? 'primary' : 'default'}/>
+                        </IconButton>
+                    </div>
+                    <div className={styles.commentBox}>
           <textarea
-            type="text"
-            id={`comment_${id}`}
-            name={`comment_${id}`}
-            placeholder="Agrega una respuesta al comentario..."
-            className={styles.inputComment}
-            onChange={(e) => setReplyText(e.target.value)}
-            value={replyText}
+              type="text"
+              id={`comment_${id}`}
+              name={`comment_${id}`}
+              placeholder="Agrega una respuesta al comentario..."
+              className={styles.inputComment}
+              onChange={handleAnswer}
+              value={replyText}
           />
-          <div className={styles.line} />
-          <Box className={styles.buttonsContainers}>
-            <button className={styles.cancelButton} onClick={handleCancel}>
-              Cancelar
-            </button>
-            <button className={styles.answerButton} onClick={handleReply}>
-              Responder
-            </button>
-          </Box>
-        </div>
+                        <div className={styles.line}/>
+                        <Box className={styles.buttonsContainers}>
+                            <button className={styles.cancelButton} onClick={handleCancel}>
+                                Cancelar
+                            </button>
+                            <button className={styles.answerButton} onClick={handleReply}>
+                                Responder
+                            </button>
+                        </Box>
+                    </div>
 
-        {/*Seccion de Respuesta a comentario */}
+                    {/*Seccion de Respuesta a comentario */}
+                    {/*{console.log(responses)}*/}
+                    {responses.map((response, index) => (
+                        <div key={index}>
+                            {replyToComment && (
+                                <div className={styles.replyToCommentContainer}>
+                                    <span className={styles.userNameResponseField}>{response?.author?.email} </span>
+                                    <Typography variant="date2">
+                                        {response.dateTime}
+                                    </Typography>
+                                </div>
 
-        {responses.map((response, index) => (
-        <div key={index}>
-          {replyToComment && (
-            <div className={styles.replyToCommentContainer}>
-              <span className={styles.userNameResponseField}>pepito@mail.com </span>
-              <Typography variant="date2">
-                {response.dateTime}
-              </Typography>
-            </div>
-
-          )}
-    <div className={styles.responseUserInfo}>
-      <Typography variant="username2">
-        {response.authorEmail}
-      </Typography>
-    </div>
-    <div className={styles.responseCommentBox}>
-              <Typography variant="body2" className={styles.text}>
-                {response.content}
-              </Typography>
-      <div className={styles.likesContainer}>
-        <IconButton aria-label="Me gusta">
-          <ThumbUpIcon />
-        </IconButton>
-        <Typography variant="Numero de likes" className={styles.likeCount}>
-          {response.likes}
-        </Typography>
-        <IconButton aria-label="No me gusta">
-          <ThumbDownIcon />
-        </IconButton>
-      </div>
-    </div>
-  </div>
-))}
-      </CardContent>
-    </Card>
-  );
+                            )}
+                            <div className={styles.responseUserInfo}>
+                                <Typography variant="username2">
+                                    {response.authorEmail}
+                                </Typography>
+                            </div>
+                            <div className={styles.responseCommentBox}>
+                                <Typography variant="body2" className={styles.text}>
+                                    {response.content}
+                                </Typography>
+                                <div className={styles.likesContainer}>
+                                    <IconButton aria-label="Me gusta">
+                                        <ThumbUpIcon/>
+                                    </IconButton>
+                                    <Typography variant="Numero de likes" className={styles.likeCount}>
+                                        {response.likes}
+                                    </Typography>
+                                    <IconButton aria-label="No me gusta">
+                                        <ThumbDownIcon/>
+                                    </IconButton>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </CardContent>
+            </Card>
+            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} TransitionComponent={Slide}
+                      anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert onClose={handleClose} severity="error">
+                    {message}
+                </Alert>
+            </Snackbar>
+        </>
+    );
 };
 
 export default CommentComponent;
--- components/QuizComents.js
+++ components/QuizComents.js
@@ -1,9 +1,11 @@
 "use client"
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from '../styles/QuizComents.module.css';
 import {useRequestService} from "@/service/request.service";
 import CommentComponent from "@/components/CommentComponent";
 import Cookies from "js-cookie";
+import {Slide, Snackbar} from "@mui/material";
+import {Alert} from "@mui/lab";
 
 const jwt = require('jsonwebtoken');
 
@@ -15,13 +17,15 @@ const QuizComents = () => {
     const service = useRequestService()
     const [quizId, setQuizId] = useState('0')
     const [replyToCommentId, setReplyToCommentId] = useState(null);
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState("ERROR");
 
     useEffect(() => {
         const id = window.location.pathname.split('/')[2]
         setQuizId(id)
         service.fetchComments(id).then(commentsList => {
             setComments(commentsList)
-        }).catch(error =>{
+        }).catch(error => {
             console.error("Error fetching comments:", error);
             setComments([])
         })
@@ -30,63 +34,44 @@ const QuizComents = () => {
     function handleComment(event) {
         setComment(event.target.value)
         event.target.style.height = '22px'
-        event.target.style.height = (event.target.scrollHeight + 1)+'px'
+        event.target.style.height = (event.target.scrollHeight + 1) + 'px'
     }
 
     function cancelComment() {
         setComment("")
+        document.getElementById("comment").style.height = '22px'
         document.getElementById("comment").value = ""
     }
 
-    // async function logComment() {
-    //     if (comment !== "") {
-    //       const data = jwt.decode(Cookies.get('jwt'));
-    //       const comData = {
-    //         content: comment,
-    //         quizId: quizId,
-    //         userId: data.id,
-    //         parentCommentId: null, // Esto indica que es un comentario principal
-    //       };
-    
-    //       if (replyToCommentId) {
-    //         comData.parentCommentId = replyToCommentId; // Si hay un comentario al que se responde, asigna su ID como parentCommentId
-    //       }
-    
-    //       const com = await service.logComment(comData);
-    //       setComments((prevState) => {
-    //         if (Array.isArray(prevState)) {
-    //           return [...prevState, com];
-    //         } else {
-    //           return [com];
-    //         }
-    //       });
-    //       cancelComment();
-    //       setReplyToCommentId(null); // Reinicia el ID del comentario al que se responde
-    //     }
-    //   }
 
     async function logComment() {
-        if (comment !== "") {
-          const data = jwt.decode(Cookies.get('jwt'));
-          const comData = {
-            content: comment,
-            quizId: quizId,
-            userId: data.id,
-            parentCommentId: replyToCommentId || null, // Asigna el ID del comentario al que se responde o null si es un comentario principal
-          };
-    
-          const com = await service.logComment(comData);
-          setComments((prevState) => {
-            if (Array.isArray(prevState)) {
-              return [...prevState, com];
-            } else {
-              return [com];
+        const trimmedComment = comment.trim();
+        if (trimmedComment !== "") {
+            if (trimmedComment.length <= 255) {
+                const data = jwt.decode(Cookies.get('jwt'));
+                const comData = {
+                    content: trimmedComment,
+                    quizId: quizId,
+                    userId: data.id,
+                    parentCommentId: replyToCommentId || null, // Asigna el ID del comentario al que se responde o null si es un comentario principal
+                };
+
+                const com = await service.logComment(comData);
+                setComments((prevState) => {
+                    if (Array.isArray(prevState)) {
+                        return [...prevState, com];
+                    } else {
+                        return [com];
+                    }
+                });
+                cancelComment();
+                setReplyToCommentId(null); // Reinicia el ID del comentario al que se responde
+            }else{
+                setMessage("El comentario no puede tener mas de 255 caracteres");
+                setOpen(true);
             }
-          });
-          cancelComment();
-          setReplyToCommentId(null); // Reinicia el ID del comentario al que se responde
         }
-      }
+    }
 
     function handleDeleteComment(index) {
         const selectedComment = comments[index]
@@ -109,6 +94,13 @@ const QuizComents = () => {
         setOpenComment(false)
     }
 
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
     return (
         <div className={styles.backgroundBox}>
             <div className={styles.componentBox}>
@@ -128,29 +120,29 @@ const QuizComents = () => {
                     <div className={styles.comentBox} id="commentBox">
                         <p className={styles.numberTextComents}>{comments.length} Comentarios</p>
                         <textarea type="text" id="comment" name="comment" placeholder="Agrega un comentario..."
-                               className={styles.inputComment} onChange={handleComment}/>
+                                  className={styles.inputComment} onChange={handleComment}/>
                         <div className={styles.insertCommentLine}/>
                         <div className={styles.buttonsContainers}>
                             <button className={styles.whiteButton} onClick={cancelComment}>Cancelar</button>
                             <button className={styles.greenButton} onClick={logComment}>Comentar</button>
                         </div>
                         <div>
-                        {comments &&
-            comments.map((comment, index) => {
-              return (
-                <CommentComponent
-                  quizId={quizId}
-                  key={comment.id}
-                  handleDeleteComment={() => handleDeleteComment(index)}
-                  id={comment.id}
-                  content={comment.content}
-                  likes={comment.likes}
-                  authorEmail={comment.author?.email}
-                  replyToComment={comment.id} // Establece el ID del comentario al que se responde
-                  replies={comment.responses}
-                />
-              );
-            })}
+                            {comments &&
+                                comments.map((comment, index) => {
+                                    return (
+                                        <CommentComponent
+                                            quizId={quizId}
+                                            key={comment.id}
+                                            handleDeleteComment={() => handleDeleteComment(index)}
+                                            id={comment.id}
+                                            content={comment.content}
+                                            likes={comment.likes}
+                                            authorEmail={comment.author?.email}
+                                            replyToComment={comment.id} // Establece el ID del comentario al que se responde
+                                            replies={comment.responses}
+                                        />
+                                    );
+                                })}
                         </div>
                     </div>
                     :
@@ -159,6 +151,12 @@ const QuizComents = () => {
                     </div>
                 }
             </div>
+            <Snackbar open={open} autoHideDuration={5000} onClose={handleClose} TransitionComponent={Slide}
+                      anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert onClose={handleClose} severity="error">
+                    {message}
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
